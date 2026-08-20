a2ac6455cb200129821d1e2babfc4b1326ea5219
kalebunna <siconfix@gmail.com>
2026-08-21 03:49:00 +0700

    fix DatabaseSeeder for production

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'admin@stainas.ac.id'],
+            [
+                'name' => 'Administrator',
+                'password' => \Illuminate\Support\Facades\Hash::make('admin123'),
+            ]
+        );
     }
 }
